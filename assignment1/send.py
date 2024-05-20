af956fc2073c57615d6f57fc3266e6ef7b9ffa21
zele24 <144828280+zele24@users.noreply.github.com>
2024-05-20 12:31:58 +0100

    Ex 1 final edit

--- assignment1/send.py
+++ assignment1/send.py
@@ -14,9 +14,9 @@ def send_random_traffic(num_packets, interface, src_ip, dst_ip):
     total_pkts = 0
     port = 5555
     for i in range(num_packets):
-            data = randomword(470)
+            data = randomword(458)
             p = Ether(dst=dst_mac,src=src_mac)/IP(dst=dst_ip,src=src_ip)
-            p = p/UDP(sport= 50000, dport=port)/Raw(load=data)
+            p = p/TCP(sport= 50000, dport=port)/Raw(load=data)
             sendp(p, iface = interface, inter = 0.01)
             # If you want to see the contents of the packet, uncomment the line below
             # print(p.show())
